9011432241048e2dfe5a1ef2e1fddfba0de67c98
Jonas Bertels <r0666113@pc-klas4-14.esat.kuleuven.be>
2020-11-09 14:41:51 +0100

    Create a MAC for 3 inputs

--- src/rtl_building_blocks/device/mac3.sv
+++ src/rtl_building_blocks/device/mac3.sv
@@ -13,8 +13,12 @@ module mac #(
   input logic input_valid,
   input logic accumulate_internal, //accumulate (accumulator <= a*b + accumulator) if high (1) or restart accumulation (accumulator <= a*b+0) if low (0)
   input logic [ACCUMULATOR_WIDTH-1:0] partial_sum_in,
-  input logic signed [A_WIDTH-1:0] a,
-  input logic signed [B_WIDTH-1:0] b,
+  input logic signed [A_WIDTH-1:0] a0,
+  input logic signed [B_WIDTH-1:0] b0,
+  input logic signed [A_WIDTH-1:0] a1,
+  input logic signed [B_WIDTH-1:0] b1,
+  input logic signed [A_WIDTH-1:0] a2,
+  input logic signed [B_WIDTH-1:0] b2,
 
   //output
   output logic signed [OUTPUT_WIDTH-1:0] out
@@ -35,33 +39,75 @@ module mac #(
                                                 clk
   */
 
-  logic signed [ACCUMULATOR_WIDTH-1:0] product;
+  logic signed [ACCUMULATOR_WIDTH-1:0] product0;
   multiplier #( .A_WIDTH(A_WIDTH),
                 .B_WIDTH(B_WIDTH),
                 .OUT_WIDTH(ACCUMULATOR_WIDTH),
                 .OUT_SCALE(0))
-    mul
-    (.a(a),
-     .b(b),
-     .out(product));
+    mul0
+    (.a(a0),
+     .b(b0),
+     .out(product0));
+
+  logic signed [ACCUMULATOR_WIDTH-1:0] product1;
+  multiplier #( .A_WIDTH(A_WIDTH),
+                .B_WIDTH(B_WIDTH),
+                .OUT_WIDTH(ACCUMULATOR_WIDTH),
+                .OUT_SCALE(0))
+    mul1
+    (.a(a1),
+     .b(b1),
+     .out(product1));
+
+  logic signed [ACCUMULATOR_WIDTH-1:0] product2;
+  multiplier #( .A_WIDTH(A_WIDTH),
+                .B_WIDTH(B_WIDTH),
+                .OUT_WIDTH(ACCUMULATOR_WIDTH),
+                .OUT_SCALE(0))
+    mul2
+    (.a(a2),
+     .b(b2),
+     .out(product2));
 
 
   //makes register with we accumulator_value_we, qout accumulator_value, din accumulator_value_next, clk clk and arst_n_in arst_n_in
   //see register.sv
   `REG(ACCUMULATOR_WIDTH, accumulator_value);
   assign accumulator_value_we = input_valid;
+  logic signed [ACCUMULATOR_WIDTH-1:0] tempSum0;
+  logic signed [ACCUMULATOR_WIDTH-1:0] tempSum1;
+
   logic signed [ACCUMULATOR_WIDTH-1:0] sum;
   assign accumulator_value_next = sum;
 
   logic signed [ACCUMULATOR_WIDTH-1:0] adder_b;
   assign adder_b = accumulate_internal ? accumulator_value : partial_sum_in;
+
   adder #( .A_WIDTH(ACCUMULATOR_WIDTH),
            .B_WIDTH(ACCUMULATOR_WIDTH),
            .OUT_WIDTH(ACCUMULATOR_WIDTH),
            .OUT_SCALE(0))
-    add
-    (.a(product),
+    add0
+    (.a(product0),
+     .b(product1),
+     .out(tempSum0));
+
+      adder #( .A_WIDTH(ACCUMULATOR_WIDTH),
+           .B_WIDTH(ACCUMULATOR_WIDTH),
+           .OUT_WIDTH(ACCUMULATOR_WIDTH),
+           .OUT_SCALE(0))
+    add1
+    (.a(product2),
      .b(adder_b),
+     .out(tempSum1));
+
+    adder #( .A_WIDTH(ACCUMULATOR_WIDTH),
+           .B_WIDTH(ACCUMULATOR_WIDTH),
+           .OUT_WIDTH(ACCUMULATOR_WIDTH),
+           .OUT_SCALE(0))
+    add2
+    (.a(tempSum0),
+     .b(tempSum1),
      .out(sum));
 
   assign out = accumulator_value >>> OUTPUT_SCALE;
