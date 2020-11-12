94163cdf85c419c859fda643a940b367cc671d03
Yoshi Vermeire <r0669247@pc-klas4-1.esat.kuleuven.be>
2020-11-12 09:26:54 +0100

    commit to pul

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,20 +1,37 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _1799850_archive_1.so
-_1799850_archive_1.so : archive.24/_1799850_archive_1.a
+ARCHIVE_OBJS += _1802296_archive_1.so
+_1802296_archive_1.so : archive.27/_1802296_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../out/simv.daidir//_1799850_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../out/simv.daidir//_1802296_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../out/simv.daidir//_1799850_archive_1.so $@
+	@ln -sf .//../out/simv.daidir//_1802296_archive_1.so $@
+
+
+ARCHIVE_OBJS += _prev_archive_1.so
+_prev_archive_1.so : archive.27/_prev_archive_1.a
+	@$(AR) -s $<
+	@$(PIC_LD) -shared  -o .//../out/simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
+	@rm -f $@
+	@ln -sf .//../out/simv.daidir//_prev_archive_1.so $@
 
 
 
+VCS_ARC0 =_csrc0.so
+
+VCS_OBJS0 =objs/amcQw_d.o 
 
 
 
 %.o: %.c
 	$(CC_CG) $(CFLAGS_CG) -c -o $@ $<
+
+$(VCS_ARC0) : $(VCS_OBJS0)
+	$(PIC_LD) -shared  -o .//../out/simv.daidir//$(VCS_ARC0) $(VCS_OBJS0)
+	rm -f $(VCS_ARC0)
+	@ln -sf .//../out/simv.daidir//$(VCS_ARC0) $(VCS_ARC0)
+
 CU_UDP_OBJS = \
 
 
@@ -22,7 +39,7 @@ CU_LVL_OBJS = \
 SIM_l.o 
 
 MAIN_OBJS = \
-objs/amcQw_d.o 
 
-CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
+
+CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(VCS_ARC0) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
 
