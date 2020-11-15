38f742b1698c17d4fb7abe60f6d3b6a3b0d59f0c
Yoshi Vermeire <r0669247@esat.kuleuven.be>
2020-11-15 15:49:33 +0100

    fix 1

--- src/test/driver.sv
+++ src/test/driver.sv
@@ -38,9 +38,8 @@ class Driver #(config_t cfg);
 	  Transaction_Kernel #(cfg) tract_kernel;
       gen2drv_feature.get(tract_feature);
       gen2drv_kernel.get(tract_kernel);
-
 	  repeat (4) @(intf_i.cb);
-	  Reset
+	  Reset;
       $display("[DRV] -----  Start execution -----");
       intf_i.cb.start <= 1;
       @(intf_i.cb);
