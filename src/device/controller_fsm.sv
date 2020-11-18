bf0dea5eac7e950a376394c9e3f1827770b62346
Yoshi Vermeire <r0669247@esat.kuleuven.be>
2020-11-18 01:25:40 +0100

    error

--- src/device/controller_fsm.sv
+++ src/device/controller_fsm.sv
@@ -170,7 +170,7 @@ module controller_fsm #(
   assign mem_we_4_we   = 1; 
   assign mem_we_5_next = mem_we_4;
   assign mem_we_5_we   = 1;
-  assign mem_we        m mem_we_5;  
+  assign mem_we        = mem_we_5;  
   /*
   chosen loop order:
   for ch_in
